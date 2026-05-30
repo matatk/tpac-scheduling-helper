2f51cbd084272badf94a788e536dadc4559d8d06
Matthew Tylee Atkinson <matatk@gmail.com>
2026-05-30 21:24:32 +0100

    fix: Move test to the right place

--- test/meeting-from-issue.test.ts
+++ test/meeting-from-issue.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'vitest'
 
 import { Temporal } from '@js-temporal/polyfill'
 
-import { _parseBodyInfo } from './src/meeting-from-issue'
+import { _parseBodyInfo } from '../src/meeting-from-issue'
 
 describe('_parseBodyInfo()', () => {
 	test('well-formed info', () => {
